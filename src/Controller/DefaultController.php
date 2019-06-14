ea99f0e2ad0d2cb2a30a30b06965552e118c721a
Olehnechyporuk <oleh.nechyporuk@gmail.com>
2019-06-14 11:42:21 +0300

    start project

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DefaultController
+{
+    public function index()
+    {
+       return new JsonResponse([55]);
+    }
+}
